4f6ad10bddec220c2f44b5b14ddc87019dfaf7de
Norbert Szulc <norbert@not7cd.net>
2020-09-11 02:34:15 +0200

    Move optfun

---
+++ src/svopt.jl
@@ -0,0 +1,30 @@
+module svopt
+export optfun
+
+ρ = (sqrt(5) - 1)/2
+
+fw1(a, b) = a + (1 - ρ)*(b - a)
+fw2(a, b) = b + (1 - ρ)*(b - a)
+
+"for simplicity x^(k-1) is xk0 and xk1 is x^(k)"
+function optfun(f, a, b; ϵ=1e-5)
+    @assert b > a
+    k = 1
+    w1 = fw1(a, b)
+    w2 = fw2(a, b)
+
+    while b - a < ϵ
+        if f(w1) < f(w2)
+            b = w2
+            w2 = w1
+            w1 = fw2(a, b)
+        else
+            a = w1
+            w1 = w2
+            w2 = fw2(a, b)
+        end
+    end
+    return a
+end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 using svopt
 using Test
 
-@testset "svopt.jl" begin
-    # Write your tests here.
+
+ϵ = 1e-5
+
+
+@testset "optfun" begin
+    f1(x) = x^2
+    f2(x) = (x+1)^4 + 10
+    f3(x) = (x+10)^2 - 10
+    @test optfun(f1, -3, 10) - 0 < ϵ
+    @test optfun(f2, -3, 10) - 1 < ϵ
+    @test optfun(f3, 3, 30) - 10 < ϵ
 end
